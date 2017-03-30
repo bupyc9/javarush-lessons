8443da4a71e73eeb3251e713ed2fb30bd03a89a9
Afanasyev Pavel <bupyc9@gmail.com>
2017-03-30 23:30:25 +0300

    Одного кота нам мало

---
+++ src/com/javarush/task/task02/task0208/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task02.task0208;
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat murzik = new Cat();
+        Cat vaska = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
