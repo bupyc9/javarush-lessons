8eb641bd5ab4ecebe1b982f915f7d4d746b97848
Afanasyev Pavel <bupyc9@gmail.com>
2017-04-01 23:21:45 +0300

    Финансовые ожидания

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0325;
+
+import java.io.*;
+
+/* 
+Финансовые ожидания
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.print("Я буду зарабатывать $" + n + " в час");
+    }
+}
