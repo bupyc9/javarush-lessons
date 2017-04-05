7f14c3f090e100151a0fc3a8f02bccb40aa9502f
Afanasyev Pavel <bupyc9@gmail.com>
2017-04-05 22:48:22 +0300

    Мужчина и женщина

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0526/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task05.task0526;
+
+/* 
+Мужчина и женщина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Man man1 = new Man("test1", 1, "address1");
+        Man man2 = new Man("test2", 2, "address2");
+
+        Woman woman1 = new Woman("test3", 3, "address3");
+        Woman woman2 = new Woman("test4", 4, "address4");
+
+        System.out.println(man1.name + " " + man1.age + " " + man1.address);
+        System.out.println(man2.name + " " + man2.age + " " + man2.address);
+
+        System.out.println(woman1.name + " " + woman1.age + " " + woman1.address);
+        System.out.println(woman2.name + " " + woman2.age + " " + woman2.address);
+    }
+
+    public static class Man {
+        String name;
+        int age;
+        String address;
+
+        Man(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+
+    public static class Woman {
+        String name;
+        int age;
+        String address;
+
+        Woman(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+    }
+}
