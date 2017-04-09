cf3b1b49966708d4fde1e282194c14b0a6952a43
Afanasyev Pavel <bupyc9@gmail.com>
2017-04-09 21:26:09 +0300

    Как назвали, так назвали

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task04.task0443;
+
+
+/* 
+Как назвали, так назвали
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = bufferedReader.readLine();
+        int y = Integer.parseInt(bufferedReader.readLine());
+        int m = Integer.parseInt(bufferedReader.readLine());
+        int d = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.print("Меня зовут " + name + ".\nЯ родился " + d + "." + m + "." + y);
+    }
+}
