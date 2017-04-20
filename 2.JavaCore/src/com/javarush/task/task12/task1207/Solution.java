963ffc596da32870a8d451c1020ce0e973a60d2c
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-04-20 23:13:28 +0300

    Int и Integer

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution.print(1);
+        Solution.print(new Integer(1));
+    }
+
+    public static void print(int s) {
+
+    }
+
+    public static void print(Integer s) {
+
+    }
+}
