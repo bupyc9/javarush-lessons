424fb26f50128ebb7a1303c7335b7ec9c210c5ee
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-04-20 23:47:23 +0300

    Лететь, бежать и плыть

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {
+        public void start();
+    }
+
+    public interface Run {
+        public void start();
+    }
+
+    public interface Swim {
+        public void start();
+    }
+}
