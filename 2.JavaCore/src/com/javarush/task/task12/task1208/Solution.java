3cd8a4020ee35008c6f0c3183cec8be626925c73
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-04-20 23:14:25 +0300

    Свобода печати

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+
+    public static void print(int s) {
+
+    }
+
+    public static void print(boolean s) {
+
+    }
+
+    public static void print(float s) {
+
+    }
+
+    public static void print(double s) {
+
+    }
+}
