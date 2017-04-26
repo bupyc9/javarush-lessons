f39745efff409458a1f0d367d9c45f3795e3c509
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-04-26 23:15:54 +0300

    Перегрузка конструкторов

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    Solution(String s) {
+
+    }
+
+    public Solution(Integer i) {
+
+    }
+
+    private Solution(Short s) {
+
+    }
+
+    protected Solution(Double d) {
+
+    }
+}
+
