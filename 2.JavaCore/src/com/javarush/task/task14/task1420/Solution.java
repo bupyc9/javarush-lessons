ddd25c2c3431a983a3099fc72f26a6140aa7d63d
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-01 18:54:48 +0300

    НОД

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task14.task1420;
+
+/* 
+НОД
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+
+        if (a <= 0 || b <= 0) {
+            throw new Exception("только положительные числа");
+        }
+
+        System.out.println(gcd(a, b));
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+
+        return a;
+    }
+}
