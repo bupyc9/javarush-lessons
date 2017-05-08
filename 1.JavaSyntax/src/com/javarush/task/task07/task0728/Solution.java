327741a5c54a0dd45fba75e58f252dc4ba1a1e34
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-09 00:46:35 +0300

    В убывающем порядке

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task07.task0728;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+В убывающем порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(int[] array) {
+        int n = array.length;
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (array[j - 1] < array[j]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+        }
+    }
+}
