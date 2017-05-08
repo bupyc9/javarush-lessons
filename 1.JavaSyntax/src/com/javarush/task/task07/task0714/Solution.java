27cca2ad553260cdad8d5b5e8592ef8a96a07306
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-08 23:14:17 +0300

    Слова в обратном порядке

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task07.task0714;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+Слова в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+        }
+
+        list.remove(2);
+        Collections.reverse(list);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
+
+
