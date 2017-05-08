9103422da4c1720e67e39a78473496c3cca170d2
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-08 18:36:11 +0300

    Класс StringHelper

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0611/StringHelper.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task06.task0611;
+
+/* 
+Класс StringHelper
+*/
+
+public class StringHelper {
+    public static String multiply(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            result.append(s);
+        }
+        return result.toString();
+    }
+
+    public static String multiply(String s, int count) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            result.append(s);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
