7ba9e7acf333459730ba61a943799ce1f323bbb7
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-10 00:36:23 +0300

    Функциональности маловато!

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1019/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task10.task1019;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Функциональности маловато!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        HashMap<String, Integer> map = new HashMap<>();
+        while (true) {
+            try {
+                int id = Integer.parseInt(reader.readLine());
+                String name = reader.readLine();
+                map.put(name, id);
+            } catch (NumberFormatException e) {
+                break;
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            System.out.println(entry.getValue() + " " + entry.getKey());
+        }
+    }
+}
