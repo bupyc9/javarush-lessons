ef5dc0ded1b8444bce6bf5bdcd37bda0a20339d2
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-09 23:43:43 +0300

    Список из массивов чисел

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task09.task0926;
+
+import java.util.ArrayList;
+
+/* 
+Список из массивов чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<int[]> list = createList();
+        printList(list);
+    }
+
+    public static ArrayList<int[]> createList() {
+        ArrayList<int[]> list = new ArrayList<>();
+        int[] lengths = {5, 2, 4, 7, 0};
+        for (int i = 0; i < lengths.length; i++) {
+            list.add(new int[lengths[i]]);
+        }
+
+        return list;
+    }
+
+    public static void printList(ArrayList<int[]> list) {
+        for (int[] array : list) {
+            for (int x : array) {
+                System.out.println(x);
+            }
+        }
+    }
+}
