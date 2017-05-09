fc6c3025eeacf7fd21642336caedd8c2ede7dc04
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-09 21:31:38 +0300

    20 слов на букву «Л»

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static HashSet<String> createSet() {
+        HashSet<String> set = new HashSet<>();
+        set.add("Лера");
+        set.add("Лера1");
+        set.add("Лера2");
+        set.add("Лера3");
+        set.add("Лера4");
+        set.add("Лера5");
+        set.add("Лера6");
+        set.add("Лера7");
+        set.add("Лера8");
+        set.add("Лера9");
+        set.add("Лера10");
+        set.add("Лера11");
+        set.add("Лера12");
+        set.add("Лера13");
+        set.add("Лера14");
+        set.add("Лера15");
+        set.add("Лера16");
+        set.add("Лера17");
+        set.add("Лера18");
+        set.add("Лера19");
+
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
