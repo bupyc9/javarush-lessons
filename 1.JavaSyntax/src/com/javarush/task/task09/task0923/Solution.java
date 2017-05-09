bcc2dae04cae87bbe3067d09b47b0cb461ae90e3
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-09 23:33:28 +0300

    Гласные и согласные

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task09.task0923;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Гласные и согласные
+*/
+
+public class Solution {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        char[] text = reader.readLine().replace(" ", "").toCharArray();
+        reader.close();
+
+        ArrayList<Character> consonant = new ArrayList<>();
+        for (Character character : text) {
+            if (isVowel(character)) {
+                System.out.print(character + " ");
+            } else {
+                consonant.add(character);
+            }
+        }
+
+        System.out.println();
+
+        for (Character character : consonant) {
+            System.out.print(character + " ");
+        }
+    }
+
+    // метод проверяет, гласная ли буква
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+
+        for (char d : vowels)   // ищем среди массива гласных
+        {
+            if (c == d)
+                return true;
+        }
+        return false;
+    }
+}
