bb78025f7f5556c6e0403d9e104f027aae3a96af
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-09 23:18:32 +0300

    Задача по алгоритмам

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0830/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task08.task0830;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        int n = array.length;
+        String temp;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+
+                if (isGreaterThan(array[j - 1], array[j])) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
